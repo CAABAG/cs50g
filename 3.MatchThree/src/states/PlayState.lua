ae5123458f492528a1661ccb17efecc9c2c31727
CAABAG <piotrbrzostowski96@gmail.com>
2025-09-03 12:07:18 +0200

    Allow for swapping on a match only

--- 3.MatchThree/src/states/PlayState.lua
+++ 3.MatchThree/src/states/PlayState.lua
@@ -144,39 +144,62 @@ function PlayState:update(dt)
                 gSounds['error']:play()
                 self.highlightedTile = nil
             else
-                
-                -- swap grid positions of tiles
-                local tempX = self.highlightedTile.gridX
-                local tempY = self.highlightedTile.gridY
-
-                local newTile = self.board.tiles[y][x]
-
-                self.highlightedTile.gridX = newTile.gridX
-                self.highlightedTile.gridY = newTile.gridY
-                newTile.gridX = tempX
-                newTile.gridY = tempY
-
-                -- swap tiles in the tiles table
-                self.board.tiles[self.highlightedTile.gridY][self.highlightedTile.gridX] =
-                    self.highlightedTile
-
-                self.board.tiles[newTile.gridY][newTile.gridX] = newTile
-
-                -- tween coordinates between the two so they swap
-                Timer.tween(0.1, {
-                    [self.highlightedTile] = {x = newTile.x, y = newTile.y},
-                    [newTile] = {x = self.highlightedTile.x, y = self.highlightedTile.y}
-                })
-                
+                local revertTileGridY = self.highlightedTile.gridY
+                local revertTileGridX = self.highlightedTile.gridX
+                self:swapTiles(y, x)
                 -- once the swap is finished, we can tween falling blocks as needed
                 :finish(function()
-                    self:calculateMatches()
+                    local swapIsMatch = false
+                    local matches = self.board:calculateMatches()
+                    if matches then
+                        for m, match in pairs(matches) do
+                            for k, tile in pairs(match) do
+                                if tile.gridY == y and tile.gridX == x then
+                                    swapIsMatch = true
+                                elseif tile.gridY == revertTileGridY and tile.gridX == revertTileGridX then
+                                    swapIsMatch = true
+                                end
+                            end
+                        end
+                    end
+                    if swapIsMatch == false then
+                        gSounds['error']:play()
+                        self:swapTiles(revertTileGridY, revertTileGridX)
+                        self.highlightedTile = nil
+                    else
+                        self:calculateMatches()
+                    end
                 end)
             end
         end
     end
 
     Timer.update(dt)
+end
+
+function PlayState:swapTiles(targetY, targetX)
+    -- swap grid positions of tiles
+    local tempX = self.highlightedTile.gridX
+    local tempY = self.highlightedTile.gridY
+
+    local newTile = self.board.tiles[targetY][targetX]
+
+    self.highlightedTile.gridX = newTile.gridX
+    self.highlightedTile.gridY = newTile.gridY
+    newTile.gridX = tempX
+    newTile.gridY = tempY
+
+    -- swap tiles in the tiles table
+    self.board.tiles[self.highlightedTile.gridY][self.highlightedTile.gridX] =
+        self.highlightedTile
+
+    self.board.tiles[newTile.gridY][newTile.gridX] = newTile
+
+    -- tween coordinates between the two so they swap
+    return Timer.tween(0.1, {
+        [self.highlightedTile] = {x = newTile.x, y = newTile.y},
+        [newTile] = {x = self.highlightedTile.x, y = self.highlightedTile.y}
+    })
 end
 
 --[[
