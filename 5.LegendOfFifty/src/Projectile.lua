8e024e916c0ce0c85467dd34320dddf90f6f476b
CAABAG <piotrbrzostowski96@gmail.com>
2026-08-24 08:12:55 +0200

    Render pots as Projectile objects

--- 5.LegendOfFifty/src/Projectile.lua
+++ 5.LegendOfFifty/src/Projectile.lua
@@ -8,15 +8,27 @@
 
 Projectile = Class{}
 
-function Projectile:init()
-    self.width = GAME_OBJECT_DEFS['pot'].width
-    self.height = GAME_OBJECT_DEFS['pot'].height
+function Projectile:init(def, x, y, direction)
+    self.texture = def.texture
+    self.frame = def.frame
+
+    self.defaultState = def.defaultState
+    self.state = self.defaultState
+    self.states = def.states
+
+    self.direction = direction
+
+    self.x = x
+    self.y = y
+    self.width = def.width
+    self.height = def.height
 end
 
 function Projectile:update(dt)
 
 end
 
-function Projectile:render()
-
+function Projectile:render(adjacentOffsetX, adjacentOffsetY)
+    love.graphics.draw(gTextures[self.texture], gFrames[self.texture][self.states[self.state].frame or self.frame],
+        self.x + adjacentOffsetX, self.y + adjacentOffsetY)
 end
--- 5.LegendOfFifty/src/states/entity/player/PlayerPotWalkState.lua
+++ 5.LegendOfFifty/src/states/entity/player/PlayerPotWalkState.lua
@@ -24,10 +24,7 @@ end
 
 function PlayerPotWalkState:update(dt)
     if love.keyboard.isDown('lctrl') or love.keyboard.isDown('space') then
-        local projectile = Projectile()
-        projectile.x = self.entity.x
-        projectile.y = self.entity.y
-        projectile.direction = self.entity.direction
+        local projectile = Projectile(GAME_OBJECT_DEFS['pot'], self.entity.x, self.entity.y, self.entity.direction)
         table.insert(self.dungeon.currentRoom.projectiles, projectile)
 
         self.entity.pickedPot = nil
